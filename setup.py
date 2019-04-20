9f5b8e7b0c0c93dd2a29aa1da56aa1b41daf4c4d
Atmaram Naik <naik.atmaram@gmail.com>
2019-04-20 15:02:13 +0530

    ATM | changed version

--- setup.py
+++ setup.py
@@ -1,18 +1,36 @@
 from setuptools import setup, find_packages
+import pathlib
 PACKAGE_NAME = 'journeypy'
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
 setup(
     name=PACKAGE_NAME,
-    version="0.0.1-beta",
+    version="0.0.1",
+    description="This Are workflow Journey library",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/atmaramnaik/journeypy.git",
     author="Atmaram Naik",
     author_email="naik_atmaram@yahoo.com",
-    description="This Journey projects",
+
     license="MIT",
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    include_package_data=True,
     classifiers=[
-        'Private :: Do Not Upload to pypi server',
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
     ],
     install_requires=[
         'future',
         'six',
     ],
+    entry_points={
+        "console_scripts": [
+            "journey=journeypy.__main__:main",  
+        ]
+    },
 )
